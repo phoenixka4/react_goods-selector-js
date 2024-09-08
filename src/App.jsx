a9912061688ad60c662280eafb0744997cb52180
Dmitry Dukyn <Phoenixka4@gmail.com>
2024-09-08 19:28:27 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState, useEffect } from 'react';
 
 export const goods = [
   'Dumplings',
@@ -14,57 +15,67 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
-    <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+export const App = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
-      <button data-cy="ClearButton" type="button" className="delete ml-3" />
-    </h1>
+  const handleRowClick = index => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button data-cy="AddButton" type="button" className="button">
-              +
-            </button>
-          </td>
+  useEffect(() => {
+    setActiveIndex(8);
+  }, []);
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+  return (
+    <main className="section container">
+      <h1
+        className={
+          activeIndex === null ? 'title is-flex is-align-items-center' : 'title'
+        }
+      >
+        {activeIndex === null
+          ? 'No goods selected'
+          : `${goods[activeIndex]} is selected`}
+        {activeIndex !== null && (
+          <button
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            onClick={() => setActiveIndex(null)}
+          />
+        )}
+      </h1>
 
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
+      <table className="table">
+        <tbody>
+          {goods.map((n, index) => (
+            <tr
+              data-cy="Good"
+              key={n}
+              className={
+                activeIndex === index ? 'has-background-success-light' : ''
+              }
             >
-              -
-            </button>
-          </td>
+              <td>
+                <button
+                  data-cy={activeIndex === index ? 'RemoveButton' : 'AddButton'}
+                  type="button"
+                  className={
+                    activeIndex === index ? 'button is-info' : 'button'
+                  }
+                  onClick={() => handleRowClick(index)}
+                >
+                  {activeIndex === index ? '-' : '+'}
+                </button>
+              </td>
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
-
-        <tr data-cy="Good">
-          <td>
-            <button data-cy="AddButton" type="button" className="button">
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+              <td data-cy="GoodTitle" className="is-vcentered">
+                {n}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+};
